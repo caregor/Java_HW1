4ac3cc121c9fd1bd647bedd31baf95ce7e8d374e
Caregor <caregor@gmail.com>
2022-11-15 12:06:40 +0300

    Made additional truth table.

--- FirstJava.java
+++ FirstJava.java
@@ -2,14 +2,18 @@ public class FirstJava {
     public static void main(String[] args) {
         boolean p = false;
         boolean q = false;
-        boolean r = false;
-        System.out.println("   p\t|   q\t|  r");
-        System.out.println("------------------------");
+        boolean resultAnd, resultOr, resultXor, resultNotQ = false;
+
+        System.out.println("   p\t|   q\t| p & q\t| p | q\t| p ^ q\t|   !q\t|");
+        System.out.println("-------------------------------------------------");
         for (int i = 0; i < 2; i++) {
             q = false;
             for (int j = 0; j < 2; j++) {
-                r = p & q;
-                System.out.printf("%b\t| %b\t| %b \n", p, q ,r);
+                resultAnd = p & q;
+                resultOr = p | q;
+                resultXor = p ^ q;
+                resultNotQ = !q;
+                System.out.printf("%b\t| %b\t| %b\t| %b\t| %b\t| %b\t| \n", p, q ,resultAnd, resultOr, resultXor, resultNotQ);
                 q = true;
             }
             p = true;
